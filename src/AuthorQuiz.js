fb3b484c8c758db169191fabeb390609803ee028
Kristi <kristithecoder@gmail.com>
2018-11-28 07:26:46 -0800

    first commit

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css';
+
+function Hero(){
+  return (<div className="row">
+    <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>Select the book written by the author shown</p>
+    </div>
+  </div>)
+}
+function Turn() {
+  return (<div/>)
+}
+
+function Continue() {
+  return (<div/>)
+}
+function Footer(){
+  return (<div id="footer" className="row">
+    <div className="col-12">
+      <p className="text-muted credit"> All Images are from <a href="http://commons.wikimedia.org/wiki/Main">Wikemedia Commons</a> and are in the public domain</p>
+    </div>
+  </div>)
+}
+
+class AuthorQuiz extends Component {
+  render() {
+    return (
+      <div className="container-fluid"> 
+        <Hero />
+        <Turn />
+        <Continue />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default AuthorQuiz;
