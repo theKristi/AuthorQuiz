5bbfcf87a87ebe3ab2c7900e39cf91509fc85390
Kristi <kristithecoder@gmail.com>
2018-12-14 07:29:17 -0800

    working on testing

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import AuthorQuiz from './AuthorQuiz';
+import Enzyme, {mount,shallow, render} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({adapter: new Adapter()});
+const state={
+  turnData: {
+    books:['The Shining', 'IT', 'David Copperfield', 'A Tale of Two Cities','Hamlet'],
+    author:{
+      name:'Charles Dickens',
+      imageUrl:'images/authors/charlesdickens.jpg',
+      imageSource:'Wikimedia Commons',
+      books:['A Tale of Two Cities', 'David Copperfield']
+    }
+
+  },
+  highlight:'none'
+}
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={()=>{}} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
